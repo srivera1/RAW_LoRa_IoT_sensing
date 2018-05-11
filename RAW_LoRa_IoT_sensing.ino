387c652794bfaef1740b85120da7351eda173db0
Sergio Rivera Lavado <srivera@ucm.es>
2018-05-11 08:38:42 +0200

    implementing communication modes

--- RAW_LoRa_IoT_sensing.ino
+++ RAW_LoRa_IoT_sensing.ino
@@ -83,11 +83,60 @@ void setup() {
     while (true);                       // if failed, do nothing
   }
   /**/
-  //Supported values are `7.8E3`, `10.4E3`, `15.6E3`, `20.8E3`, `31.25E3`, `41.7E3`, `62.5E3`, `125E3`, and `250E3`.
-  LoRa.setSignalBandwidth(62.5E3);
-  LoRa.setSpreadingFactor(7);           // ranges from 6-12,default 7 see API docs
+  // TODO: implement more modes
+  int Mode = 0;
+  if (Mode == 0) // 43 bps
+    LoRa.setSignalBandwidth(20.8E3);
+  LoRa.setSpreadingFactor(11);           // ranges from 6-12,default 7 see API docs
   LoRa.setCodingRate4(8);
   LoRa.setPreambleLength(17);
+  elseif (Mode == 1) // 1476 bps
+  LoRa.setSignalBandwidth(20.8E3);
+  LoRa.setSpreadingFactor(6);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 2) // 915 bps
+  LoRa.setSignalBandwidth(62.5E3);
+  LoRa.setSpreadingFactor(8);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(8);
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 3) // 8509 bps
+  LoRa.setSignalBandwidth(250.0E3);
+  LoRa.setSpreadingFactor(7);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(5); // check this value
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 4) // 17738 bps
+  LoRa.setSignalBandwidth(250.0E3);
+  LoRa.setSpreadingFactor(6);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 5) // 104 bps
+  LoRa.setSignalBandwidth(41.7E3);
+  LoRa.setSpreadingFactor(11);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(8);
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 6) // 2959 bps
+  LoRa.setSignalBandwidth(41.7E3);
+  LoRa.setSpreadingFactor(6);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 7) // 841 bps
+  LoRa.setSignalBandwidth(20.8E3);
+  LoRa.setSpreadingFactor(7);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 8) // 4435 bps
+  LoRa.setSignalBandwidth(62.5E3);
+  LoRa.setSpreadingFactor(6);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(17);
+  elseif (Mode == 9)
+  // not supported
+  LoRa.setSignalBandwidth(500.0E3);
+  LoRa.setSpreadingFactor(6);           // ranges from 6-12,default 7 see API docs
+  LoRa.setCodingRate4(5);
+  LoRa.setPreambleLength(17);
+  end;
   /**/
   Serial.println("LoRa init succeeded.");
 }
@@ -208,10 +257,10 @@ void loopWeather() {
   content.concat(",");*/
   int contentSize = content.length();
   // FEC, error correction "best 2 out of 3"
-  
+
   content = String(contentSize) + ';' + content + content + content;
   // TODO: implementation of Hamming codes or Reed-Solomon codes
-  
+
   while (content.length() < 256)
     content.concat(".");
 
